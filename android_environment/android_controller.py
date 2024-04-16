4dff409f580ebd9f41659fcea9fa5c8df144d248
Krish Matta <self@krishxmatta.dev>
2024-04-16 19:21:16 -0400

    Implement tap functionality

--- android_environment/android_controller.py
+++ android_environment/android_controller.py
@@ -20,6 +20,9 @@ class AndroidController:
         ret = execute_command(f"adb -s {self.device} shell wm size")
         return map(int, ret.split(": ")[1].split("x"))
 
+    def tap(self, pos):
+        return execute_command(f"adb -s {self.device} shell input tap {pos[0]} {pos[1]}")
+
     def swipe_point(self, pos, direction, length=2, duration=400):
         """
         Swipes on the screen, starting at pos and towards the given direction for the specified length and time duration.
